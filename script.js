d91388d52d7f4584012d807be7ca2abd3ce250d4
haseebcheema <ihaseebtariq1@gmail.com>
2024-03-09 20:49:10 +0500

    animated few things of page1 and did some changes in the code

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 
-// used locomotive for smooth scrolling effect
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
-
 // the locomotive scroll hijacks the scroll effect of gsap
-// so we'll use locomotive with scrolltrigger which is incorporated below in the init function
+// so we'll use locomotive with ScrollTrigger which is incorporated below in the init function
 
 // function
 function init(){
     gsap.registerPlugin(ScrollTrigger);
 
-// --- SETUP START ---
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.defaults({ scroller: ".main" });
-// --- SETUP END ---
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
+    ScrollTrigger.refresh();
 }
 
 // function calling
 init()
+
+// used gsap for animations 
+
+var tl= gsap.timeline({
+    scrollTrigger: {
+                trigger: ".page1>h1",
+                scroller: ".main",
+                markers: true,
+                start: "top 23%",
+                end: "top 0",
+                scrub: 3
+            }
+})
+
+// animate h1
+tl.to(".page1>h1",{
+    x: -100
+})
+
+// animate h2
+tl.to(".page1>h2",{
+    x: 100
+})
+
+// animate video
+tl.to(".page1 video",{
+    width: "98%"
+})
